2f7995dafbaf4a1bfc68012e165afc98ef2dfe78
aleacevedo <ale.acevedo@live.com.ar>
2022-12-22 23:35:50 -0300

    feat: use routers

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
-const app = express();
-app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+const models = require('./db')
+const router = require('./routers');
 
-/**
- * FIX ME!
- * @returns contract by id
- */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+const app = express();
+
+app.set('models', models)
+app.use(bodyParser.json());
+app.use(router);
+
 module.exports = app;
